e04820e7b4fa5ef98e458c17355afcb17261f809
tanishabisht <agamyatani@gmail.com>
2021-07-19 11:56:38 +0530

    create :: basic project structure

---
+++ packages/frontend/src/Pages/MyParticipations/MyParticipations.js
@@ -0,0 +1,10 @@
+// import classes from './MyParticipations.module.css'
+
+const MyParticipationsPage = () => {
+    return (
+        <h1>This is MyParticipations Page</h1>
+    )
+}
+
+
+export default MyParticipationsPage
---
+++ packages/frontend/src/Pages/index.js
@@ -0,0 +1,15 @@
+export { default as Profile } from './Profile/Profile'
+export { default as Dashboard } from './Dashboard/Dashboard'
+export { default as Analytics } from './Analytics/Analytics'
+export { default as HostEventForm } from './HostEventForm/HostEventForm'
+
+// Find
+export { default as Communities } from './Communities/Communities'
+export { default as CommunityDesc } from './Communities/CommunityDesc'
+export { default as Events } from './Events/Events'
+export { default as EventDesc } from './Events/EventDesc'
+
+// Manage
+export { default as MyParticipations } from './MyParticipations/MyParticipations'
+export { default as MyCommunities } from './MyCommunities/MyCommunities'
+export { default as CreateCommunity } from './CreateCommunity/CreateCommunity'
