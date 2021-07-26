ad3d4a92c3393e9189c12442e19815be156b0e54
Tanisha Bisht <agamyatani@gmail.com>
2021-07-26 16:44:02 +0530

    landing page

--- packages/frontend/src/Pages/index.js
+++ packages/frontend/src/Pages/index.js
@@ -1,15 +1,17 @@
-export { default as Profile } from './Profile/Profile'
-export { default as Dashboard } from './Dashboard/Dashboard'
-export { default as Analytics } from './Analytics/Analytics'
-export { default as HostEventForm } from './HostEventForm/HostEventForm'
+export { default as LandingPage } from './Landing/Landing'
+
+export { default as ProfilePage } from './Profile/Profile'
+export { default as DashboardPage } from './Dashboard/Dashboard'
+export { default as AnalyticsPage } from './Analytics/Analytics'
+export { default as HostEventFormPage } from './HostEventForm/HostEventForm'
 
 // Find
-export { default as Communities } from './Communities/Communities'
-export { default as CommunityDesc } from './Communities/CommunityDesc'
-export { default as Events } from './Events/Events'
-export { default as EventDesc } from './Events/EventDesc'
+export { default as CommunitiesPage } from './Communities/Communities'
+export { default as CommunityDescPage } from './Communities/CommunityDesc'
+export { default as EventsPage } from './Events/Events'
+export { default as EventDescPage } from './Events/EventDesc'
 
 // Manage
-export { default as MyParticipations } from './MyParticipations/MyParticipations'
-export { default as MyCommunities } from './MyCommunities/MyCommunities'
-export { default as CreateCommunity } from './CreateCommunity/CreateCommunity'
+export { default as MyParticipationsPage } from './MyParticipations/MyParticipations'
+export { default as MyCommunitiesPage } from './MyCommunities/MyCommunities'
+export { default as CreateCommunityPage } from './CreateCommunity/CreateCommunity'
